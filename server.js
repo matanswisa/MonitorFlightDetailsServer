2ac4b32611aba3803f8119a8af0a64b9cc949e42
matan swisa <matanswisa01@gmail.com>
2020-12-10 10:03:14 +0200

    Added socket.io to allow socket client connect

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('server')
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
+const PORT = 8080;
+const server = http.createServer(app);
+const io = socketIo(server,{
+    cors:{
+        origin:'*',
+    }
+});
+
+io.on("connection", socket=>{
+    console.log(`new client: ${socket.client.id}`);
+
+
+    socket.emit('output',{data:'hello'});
+
+    socket.on('disconnect' , ()=>{
+        console.log(`socket disconnected:${socket.client.id}`);
+    })
+})
+
+
+server.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
