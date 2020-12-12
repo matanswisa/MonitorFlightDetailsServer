1d479289496ed4bcd834533db7eb899be34ac768
matan swisa <matanswisa01@gmail.com>
2020-12-12 12:17:25 +0200

    Input flight cords and sending the data to client

---
+++ classes/FlightCords.js
@@ -0,0 +1,9 @@
+class FlightCords {
+    constructor(altitude,his,adi){
+        this.altitude = altitude;
+        this.his = his;
+        this.adi = adi;
+    }
+}
+
+module.exports = FlightCords;
--- server.js
+++ server.js
@@ -1,27 +1,65 @@
 const express = require('express');
+const prompt = require('prompt-sync')({ sigint: true });
+const pressAnyKey = require('press-any-key');
+const FlightCords = require('./classes/FlightCords');
 const app = express();
 const http = require('http');
 const socketIo = require('socket.io');
 const PORT = 8080;
 const server = http.createServer(app);
-const io = socketIo(server,{
-    cors:{
-        origin:'*',
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
     }
 });
 
-io.on("connection", socket=>{
+
+
+
+
+function sendHeartbeat(){
+    setTimeout(sendHeartbeat, 8000);
+    io.sockets.emit('ping', { beat : 1 });
+}
+
+
+
+
+io.on("connection",  socket => {
     console.log(`new client: ${socket.client.id}`);
 
 
-    socket.emit('output',{data:'hello'});
+      socket.on('requestingCords', () => {
+        console.log('requesting data for:' + socket.client.id);
+        inputFlightCords(socket);
+    });
 
-    socket.on('disconnect' , ()=>{
+    socket.on('disconnect', () => {
         console.log(`socket disconnected:${socket.client.id}`);
     })
-})
+});
 
 
-server.listen(PORT, ()=>{
+
+server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
+
+
+const inputFlightCords = async (socket) => {
+
+    let altitude = prompt('altitude:');
+    let his = prompt('his:');
+    let adi = prompt('adi:');
+
+    let flightCords = new FlightCords(altitude, his, adi);
+    await pressAnyKey("Press any key", {
+        ctrlC: "reject"
+    }).then(() => {
+        console.log('Sending data');
+        socket.emit('cords', flightCords , {beat:1});
+    })
+        .catch(() => {
+            console.log('You pressed CTRL+C')
+        })
+}
